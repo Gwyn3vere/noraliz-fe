75f17dde593bd14e3d6b6f9e7605752f56e218f2
Gwyn3vere <tangphuc85@gmail.com>
2026-06-18 18:23:29 +0700

    fix(dashboard): prevent empty state flash while loading projects

--- src/pages/Dashboard/Projects.tsx
+++ src/pages/Dashboard/Projects.tsx
@@ -74,7 +74,7 @@ export default function Projects() {
           <Control
             tabname="Projects"
             total={projects?.length}
-            editedTime={formatEditedTime(latestProject.updatedAt)}
+            editedTime={formatEditedTime(latestProject?.updatedAt)}
             button={
               <Button className={cn(styles.emptyButton, "w-full")} onClick={handleCreateProject}>
                 <CirclePlus strokeWidth={3} />
